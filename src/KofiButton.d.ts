e53d656628a513b18de73d7a76fd3aaf67626d49
Logan Kuzyk <logan@kuzyk.net>
2021-07-13 13:16:24 -0700

    Use JavaScript with type declarations

---
+++ src/KofiButton.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function KofiButton(
+  title: string,
+  color: string,
+  kofiID: string
+): React.Component;
--- src/KofiButton.js
+++ src/KofiButton.js
@@ -1,16 +1,6 @@
 import React from "react";
 
-interface KofiButtonProps {
-  title: string;
-  color: string;
-  kofiID: string;
-}
-
-export const KofiButton: React.FC<KofiButtonProps> = ({
-  title,
-  color,
-  kofiID,
-}) => {
+export const KofiButton = ({ title, color, kofiID }) => {
   const { kofiwidget2 } = require("../kofiWidget2");
 
   kofiwidget2.init(title, color, kofiID);
