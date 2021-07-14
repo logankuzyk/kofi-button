1569274dae7f6641c1907faae800e91f40e7b168
Logan Kuzyk <logan@kuzyk.net>
2021-07-14 10:27:14 -0700

    1.0.7

--- src/KofiButton.js
+++ src/KofiButton.js
@@ -3,11 +3,13 @@ import React from "react";
 const KofiButton = ({ title, color, kofiID }) => {
   const { kofiwidget2 } = require("./kofiWidget2");
 
-  kofiwidget2.init(title, color, kofiID);
+  const KofiWidget2 = kofiwidget2();
+
+  KofiWidget2.init(title, color, kofiID);
 
   return React.createElement(
-    div,
-    { dangerouslySetInnerHTML: { __html: kofiwidget2.getHTML() } },
+    "div",
+    { dangerouslySetInnerHTML: { __html: KofiWidget2.getHTML() } },
     null
   );
 };
