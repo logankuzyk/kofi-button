6c6546488348459fced026c1cbc709447ba24860
Logan Kuzyk <logan@kuzyk.net>
2021-07-14 09:48:07 -0700

    Update type declarations

--- src/KofiButton.d.ts
+++ src/KofiButton.d.ts
@@ -1,11 +1,11 @@
 import React from "react";
 
-declare module KofiButton {
-  function KofiButton(
-    title: string,
-    color: string,
-    kofiID: string
-  ): React.Component;
-
-  export default KofiButton;
+interface KofiButtonProps {
+  title: string;
+  color: string;
+  kofiID: string;
 }
+
+declare const KofiButton: React.Component<KofiButtonProps>;
+
+export default KofiButton;
